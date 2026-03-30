fe33044ed1418bb8a771363db808793bdf27dc20
S1rt0n <glebstarikov228@gmail.com>
2026-03-30 09:25:23 +0300

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -71,7 +71,7 @@ function App() {
   // 4. ОТОБРАЖЕНИЕ (Интерфейс)
   return (
     <div style={appStyle}>
-      <h1 style={{ color: '#00695c', fontSize: '36px', fontWeight: '900',minHeight: '100vh' }}> MedHelper</h1>
+      <h1 style={{ color: '#00695c', fontSize: '36px', fontWeight: '900',minHeight: '100vh', display: 'flex', flexDirection: 'column', padding: '20px' }}> MedHelper</h1>
 
     {/* ЭКРАН 1: ПРИВЕТСТВИЕ И ФАКУЛЬТЕТЫ */}
       {!selectedFaculty && (
